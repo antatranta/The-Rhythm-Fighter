5c861a7d90eb6dd603c250faf569aa1f06a8d28f
antatranta <a_tran38@u.pacific.edu>
2019-04-03 17:40:35 -0700

    Added comments

--- Assets/Scripts/Gameplay/SongManager.cs
+++ Assets/Scripts/Gameplay/SongManager.cs
@@ -6,26 +6,34 @@ using UnityEngine;
 public class SongManager : MonoBehaviour
 {
     public float bpm;
+    // the current position of the song in seconds
     public float songPosition;
+    // the current position of the song in beats
     public float songPosInBeats;
+    // the duration of a beat (better known as a crotchet)
     public float secPerBeat;
+    // how much time has passed since the song started
     private float dspTimeSong;
     private float offset = 0.2f;
+    // the actual song / audio source
+    private AudioSource song;
 
     // Start is called before the first frame update
     public void Start()
     {
+        song = GetComponent<AudioSource>();
+
         secPerBeat = 60f / bpm;
 
         dspTimeSong = (float)AudioSettings.dspTime;
 
-        GetComponent<AudioSource>().Play();
+        song.Play();
     }
 
     // Update is called once per frame
     public void Update()
     {
-        songPosition = (float)(AudioSettings.dspTime - dspTimeSong) - offset;
+        songPosition = (float)(AudioSettings.dspTime - dspTimeSong) * song.pitch - offset;
 
         songPosInBeats = songPosition / secPerBeat;
     }
