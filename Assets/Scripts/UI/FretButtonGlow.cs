d05bcc7145050ea7d72cbedd9ab88b24d6024167
antatranta <a_tran38@u.pacific.edu>
2019-04-07 14:50:13 -0700

    Fret note now glows

---
+++ Assets/Scripts/UI/FretButtonGlow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FretButtonGlow : MonoBehaviour
+{
+    public KeyCode keyPressed;
+
+    private SpriteRenderer glowRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        glowRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyPressed))
+        {
+            glowRenderer.color = new Color(1f, 1f, 1f, 0.35f);
+        }
+        if (Input.GetKeyUp(keyPressed))
+        {
+            glowRenderer.color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+}
