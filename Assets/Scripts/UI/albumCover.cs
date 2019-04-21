b7d51cafdb0ce4f8a936ffafe175bd4c170c9f6b
Aarondip Singh <a_singh14@u.pacific.edu>
2019-04-21 15:24:34 -0700

    Some work on Song Selection Screen

---
+++ Assets/Scripts/UI/albumCover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class albumCover : MonoBehaviour
+{
+    public SongInfo songInfo;
+    public GameObject album;
+    Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.sprite = songInfo.songImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/songSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class songSelection : MonoBehaviour
+{
+    public SongInfo songInfo;
+    private SongInfoMessenger songInfoMessenger;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.text = songInfo.songTitle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
