0e1fd40b50d1e1d271bb5871b587aa0e832bb7a1
Aarondip Singh <a_singh14@u.pacific.edu>
2019-04-29 20:03:12 -0700

    Small Changes, added back button

---
+++ Assets/Scripts/UI/Song Selection/back.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back : MonoBehaviour
+{
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("Scenes/Menu");
+    }
+    public void gang()
+    {
+        
+    }
+    
+}
