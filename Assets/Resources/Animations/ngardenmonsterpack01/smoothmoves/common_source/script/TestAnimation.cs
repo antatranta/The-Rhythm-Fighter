1e638dab00cdff728a1e152e7cdd45ec8b5daa16
Shawn Elpuz <s_elpuz@u.pacific.edu>
2019-04-30 07:05:15 -0700

    Added animations to game scene

---
+++ Assets/Resources/Animations/ngardenmonsterpack01/smoothmoves/common_source/script/TestAnimation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimation : MonoBehaviour
+{
+	public string no;
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Keypad9) == true) StartCoroutine("cheatAniPlay", no);
+		if (Input.GetKeyDown(KeyCode.Keypad8) == true) gameObject.GetComponent<SmoothMoves.BoneAnimation>().Stop();
+		if (Input.GetKeyDown(KeyCode.Keypad7) == true) gameObject.GetComponent<SmoothMoves.BoneAnimation>().CrossFade("Atk_" + no);
+		if (Input.GetKeyDown(KeyCode.Keypad6) == true) gameObject.GetComponent<SmoothMoves.BoneAnimation>().CrossFade("Idle_" + no);
+	}
+	IEnumerator cheatAniPlay(string no)
+	{
+		SmoothMoves.BoneAnimation boneAni = gameObject.GetComponent<SmoothMoves.BoneAnimation>();
+
+		if (string.IsNullOrEmpty (no) == true)
+		{
+			string aniName = boneAni.GetAnimationClipName (0);
+			no = aniName.Split ('_') [1];
+		}
+		string ani = "Atk_" + no;
+		int i = boneAni.GetAnimationClipIndex(ani);
+		
+		if (i == -1) yield break;
+		
+		if (boneAni.IsPlaying(ani) == true) boneAni.Stop();
+		
+		Debug.Log(boneAni[ani].length);
+		
+		boneAni.CrossFade(ani);
+		
+		yield return new WaitForSeconds(boneAni[ani].length);
+		
+		if (boneAni != null)
+			boneAni.CrossFade("Idle_" + no);
+	}
+}
---
+++ Assets/Resources/Animations/ngardenmonsterpack01/smoothmoves/readme.txt
@@ -0,0 +1,23 @@
+SmoothMoves Character Animation
+(part no.0001~0135)
+									by Ngarden Soft
+
+Requires Unity 4.1.2 or higher / Smooth Moves Extension V2 or higher
+
+Character Animation for your games.
+
+Animation Clips
+- Idle Clip
+- Attack Clip(Attack timing is 0.5 sec)
+
+You can check the character animation in the sample scene.
+-Play scene
+-In the game window by pressing a few keys, this function operates.
+-Numpad 1~5 : If multiple characters included in the one anim, you can replace by pressing the key.
+-Numpad 6 : Play idle animation
+-Numpad 7 : Play attack animation
+-Numpad 8 : Stop animation
+-Numpad 9 : First, came after attack animation, waiting animation is playing.
+
+
+
